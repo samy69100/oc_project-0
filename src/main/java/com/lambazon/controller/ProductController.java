99bece1738f4c201db59969e42c863b7d6ae88b0
Samy <samy.infonie@infonie.fr>
2018-08-28 06:11:50 +0200

    Correction total Verif OK

--- src/main/java/com/lambazon/controller/ProductController.java
+++ src/main/java/com/lambazon/controller/ProductController.java
@@ -13,26 +13,30 @@ import com.lambazon.service.ProductService;
 
 @Controller
 public class ProductController {
-	
+
 	@Inject
 	private ProductService productService;
-	
+
 	@GetMapping("/products")
 	public String products	(Model model) {
 		model.addAttribute("prods", productService.products());
 		model.addAttribute("totalInventoryAmount", calculateTotalInventoryAmount());
 		return "products";
 	}
-	
+
 	@GetMapping("/products/{id}/details")
 	public String product	(@PathVariable Integer id, Model model) {
 		model.addAttribute("prod", productService.product(id));
 		return "product";
 	}
-	
+
 	private double calculateTotalInventoryAmount() {
-		// TODO fix calculation
-		 return 123456.78;
-		
+		// calculation fix
+		double totalInventoryAmount = 0.0f;			// storing total and retrun to end
+		for( Product ps : productService.products() ) {			// all products
+			totalInventoryAmount += ps.getInventoryPrice();		// total value and add total
+		}
+
+		return totalInventoryAmount;			// the result
 	}
 }
